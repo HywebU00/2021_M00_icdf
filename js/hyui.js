9bdadd3da2153fa79d13c8427d21cce594530dd3
anniewu <annie@hyweb.com.tw>
2023-09-07 17:26:42 +0800

    20230907修改hyui.js

--- js/hyui.js
+++ js/hyui.js
@@ -246,11 +246,11 @@ $(function() {
             // 副選單滑出
             liHasChild2.on({
                 mouseenter: function() {
-                    $(this).children('ul').stop(true, false).fadeIn();
+                    $(this).children('ul').stop(true, false).fadeIn(200);
                 },
                 mouseleave: function() {
                     $(this).parent().siblings('ul').hide();
-                    $(this).children('ul').stop(true, false).fadeOut();
+                    $(this).children('ul').stop(true, false).fadeOut(200);
                 },
             });
             liHasChild2.off('click');
@@ -259,11 +259,11 @@ $(function() {
             if (_menu.lenght > 0) {
                 liHasChild.on({
                     mouseenter: function() {
-                        $(this).children('ul').stop(true, false).fadeIn();
+                        $(this).children('ul').stop(true, false).fadeIn(200);
                     },
                     mouseleave: function() {
                         $(this).parent().siblings('ul').hide();
-                        $(this).children('ul').stop(true, false).fadeOut();
+                        $(this).children('ul').stop(true, false).fadeOut(200);
                     },
                 });
             }
@@ -271,11 +271,11 @@ $(function() {
             if (_megamenu.lenght > 0) {
                 liHasChild2.on({
                     mouseenter: function() {
-                        $(this).children('ul').stop(true, false).fadeIn();
+                        $(this).children('ul').stop(true, false).fadeIn(200);
                     },
                     mouseleave: function() {
                         $(this).parent().siblings('ul').hide();
-                        $(this).children('ul').stop(true, false).fadeOut();
+                        $(this).children('ul').stop(true, false).fadeOut(200);
                     },
                 });
             }
